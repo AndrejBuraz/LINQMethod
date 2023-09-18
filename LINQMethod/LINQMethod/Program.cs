379d29813b9d69d01a9629bd65e5a4c594318496
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-09-18 10:52:56 +0200

    Add files via upload

---
+++ LINQMethod/LINQMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQMethod
+{
+    internal class Program
+    {
+
+		public class Student
+		{
+
+			public int StudentID { get; set; }
+			public string StudentName { get; set; }
+		}
+
+		static void Main(string[] args)
+        {
+				// Student collection
+				List<Student> studentList = new List<Student>() {
+				new Student() { StudentID = 1, StudentName = "John" } ,
+				new Student() { StudentID = 2, StudentName = "Moin" } ,
+				new Student() { StudentID = 3, StudentName = "Bill" } ,
+				new Student() { StudentID = 4, StudentName = "Ram" } ,
+				new Student() { StudentID = 5, StudentName = "Ron"  }
+			};
+
+			// upit i izvršavanje upita
+			var selectResult = studentList.Select(student => student.StudentName);
+
+
+				foreach (var student in studentList)
+				{
+					Console.WriteLine(student.StudentName);
+				}
+
+				Console.ReadKey();
+			}
+    }
+}
